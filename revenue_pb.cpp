5a0c08770c1efc6d2e44b4b72396ce95df4080b7
PieFlavr <lucasadampinto@gmail.com>
2025-04-22 14:03:18 -0700

    Added header comment.

--- revenue_pb.cpp
+++ revenue_pb.cpp
@@ -1,3 +1,14 @@
+/**
+ * @file revenue_pb.cpp
+ * @author Lucas Pinto
+ * @brief Solution to HW6 problems from CS477. 
+ * @version 0.1
+ * @date 2025-04-22
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
 #include <iostream>
 #include <string>
 
